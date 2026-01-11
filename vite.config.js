27c1939a343248472eded06e83648dda1dba2eb9
Azul-Ferreyra <azul.r.ferreyra@gmail.com>
2026-01-11 16:42:43 +0100

    fix: establecer base path fijo para reparar imagenes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/github-pages/' : '/', // Base path solo para producción
+  base: '/github-pages/', // Base path solo para producción
   build: {
     // Minificar y optimizar
     minify: 'terser',
